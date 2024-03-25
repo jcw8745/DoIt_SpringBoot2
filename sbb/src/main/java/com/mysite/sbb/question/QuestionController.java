d30d51f41a2f41a114bfd89ce43f41adffd84356
DESKTOP-UBAVOGO\jcw <cjfdnjs0111@nate.com>
2024-03-25 23:16:25 +0900

    2024-03-25 180page 완료

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mysite.sbb.answer.AnswerForm;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -31,12 +36,30 @@ public class QuestionController {
 	
 	@GetMapping("/detail/{id}")
 //	@ResponseBody
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question",question);
 		
 		return "question_detail";
+	}
+	
+	@GetMapping("/create")
+//	@ResponseBody
+	public String create(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+//	@ResponseBody
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(),questionForm.getContent());
+		
+		return "redirect:/question/list"; //질문 저장 후 질문 목록으로 이동
 	}
 
 }
--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,10 +1,12 @@
 package com.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import com.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +30,14 @@ public class QuestionService {
 			throw new DataNotFoundException("question not found");
 		}
 		
+	}
+	
+	public void create(String subject, String content) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
 	}
 
 }
