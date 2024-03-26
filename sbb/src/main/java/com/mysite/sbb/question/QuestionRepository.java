c3e7926fcf33e266d33b23b59a624ef31a39524f
DESKTOP-UBAVOGO\jcw <cjfdnjs0111@nate.com>
2024-03-26 23:01:18 +0900

    2024-03-26  187page - 페이징 진행중

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,12 +2,22 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	
+	//타이틀로 단건 찾기
 	Question findBySubject(String subjString);
+	
+	// 타이틀, 내용으로 찾기
 	Question findBySubjectAndContent(String subjString , String Content);
+	
+	//타이틀로 찾기
 	List<Question> findBySubjectLike(String subject);
+	
+	//페이징
+	Page<Question> findAll(Pageable pageable);
 
 }
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,19 +1,12 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
-import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 
 import jakarta.transaction.Transactional;
 
@@ -25,9 +18,12 @@ class SbbApplicationTests {
 	private QuestionRepository questionRepository;
 	
 	@Autowired
+	private QuestionService questionService;
+	
+	@Autowired
 	private AnswerRepository answerRepository;
 	
-	@Transactional
+//	@Transactional
 	@Test
 	void testJpa() {
 		
@@ -115,14 +111,21 @@ class SbbApplicationTests {
 		 * Answer a = oa.get(); assertEquals(2, a.getQuestion().getId());
 		 */
 		
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
+		/*
+		 * Optional<Question> oq = this.questionRepository.findById(2);
+		 * assertTrue(oq.isPresent()); Question q = oq.get();
+		 * 
+		 * List<Answer> answerList = q.getAnswerList();
+		 * 
+		 * assertEquals(1, answerList.size());
+		 * assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
+		 */
 		
-		List<Answer> answerList = q.getAnswerList();
-		
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
+		for(int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]",i);
+			String content = "내용 없음";
+			this.questionService.create(subject, content);
+		}
 		
 		
 	}
